33ddb2c7e9958ea8b9a821aaf99c3db5ad80d196
Sergio <sergioalot@outlook.es>
2019-10-30 09:13:37 +0100

    add files

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
