4b87828e77c1cc7815f051fc2c16240c891a30e5
Sergio <sergioalot@outlook.es>
2019-11-14 10:03:08 +0100

    static root

--- api/views.py
+++ api/views.py
@@ -4,10 +4,13 @@ import pendulum
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.utils.timezone import now
+
 
 import json
 
 import requests
+
 
 from api.services import getapi
 from api.handler import Handler
@@ -20,18 +23,16 @@ class MyView(View):
         city = request.GET['city']
         day = self._sanitize_day(request.GET.get('day', None))
 
-
-        
-        # fechaAPI = data["list"][0]["dt_txt"].split()
-        
-
-        # TODO: convertir el date de string a clase python    
         
         handler = Handler()
+
+        modelCity = models.City.objects.get(name = city.capitalize())
+        modelDay = models.Day.objects.get(date=day, city=modelCity)
         
-        if(handler.check_db(city.capitalize(), day)):
-             records = handler.get_weather_records(city.capitalize(), day)
-             return HttpResponse(json.dumps(records))
+        if(handler.check_db(city.capitalize(), day) or (modelDay.update_at.hour - now().hour)<=2):
+            records = handler.get_weather_records(city.capitalize(), day)
+            self._update_day(day, modelCity)
+            return HttpResponse(json.dumps(records))
         else:
             r=getapi(city)
             data = json.loads(r)
@@ -52,3 +53,10 @@ class MyView(View):
             return pendulum.from_format(day, "YYYY-MM-DD")
         except:
             return dt.date.today()
+
+    def _update_day(self, fecha, city):
+        dayupdate, created = models.Day.objects.update_or_create(
+            date=fecha,
+            city=city,
+            defaults={'update_at': now()}
+        )
